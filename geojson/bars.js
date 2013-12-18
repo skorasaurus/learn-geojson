1ddea917cb9b98c3f44ea9246c20efeac965de1a
Will Skora <skorasaurus@gmail.com>
2013-12-18 18:04:55 -0500

    point to right bars file

---
+++ geojson/bars.js
@@ -0,0 +1,334 @@
+var bars = {"type": "FeatureCollection",
+"features": [
+{"type": "Feature",
+	"properties": {
+		"amenity": "bar"
+	},
+	"geometry": {"type": "Point", "coordinates": [-81.6497142, 41.489978]}},
+{"type": "Feature",
+	"properties": {
+		"name": "Tombstone Tavern",
+		"amenity": "bar"
+	},
+	"geometry": {"type": "Point", "coordinates": [-81.7092599, 41.4780971]}},
+{"type": "Feature",
+	"properties": {
+		"name": "The Brick House",
+		"amenity": "bar"
+	},
+	"geometry": {"type": "Point", "coordinates": [-81.6499155, 41.4482015]}},
+{"type": "Feature",
+	"properties": {
+		"name": "McG's",
+		"amenity": "bar"
+	},
+	"geometry": {"type": "Point", "coordinates": [-81.7335631, 41.4397674]}},
+{"type": "Feature",
+	"properties": {
+		"name": "Hopp-Inn Lounge",
+		"amenity": "bar"
+	},
+	"geometry": {"type": "Point", "coordinates": [-81.7215238, 41.4247216]}},
+{"type": "Feature",
+	"properties": {
+		"name": "WXYZ Bar",
+		"amenity": "bar",
+		"operator": "A-Loft"
+	},
+	"geometry": {"type": "Point", "coordinates": [-81.7036718, 41.5004231]}},
+{"type": "Feature",
+	"properties": {
+		"name": "Brown's Tavern",
+		"amenity": "bar"
+	},
+	"geometry": {"type": "Point", "coordinates": [-81.6536974, 41.4484709]}},
+{"type": "Feature",
+	"properties": {
+		"name": "Mr. Peabody's",
+		"amenity": "bar"
+	},
+	"geometry": {"type": "Point", "coordinates": [-81.7215719, 41.4238891]}},
+{"type": "Feature",
+	"properties": {
+		"name": "Wilbert's",
+		"amenity": "bar"
+	},
+	"geometry": {"type": "Point", "coordinates": [-81.6856973, 41.4984219]}},
+{"type": "Feature",
+	"properties": {
+		"name": "Broadview Bar",
+		"amenity": "bar"
+	},
+	"geometry": {"type": "Point", "coordinates": [-81.7034759, 41.4410723]}},
+{"type": "Feature",
+	"properties": {
+		"name": "Icebox Lounge",
+		"amenity": "bar"
+	},
+	"geometry": {"type": "Point", "coordinates": [-81.6995389, 41.4701844]}},
+{"type": "Feature",
+	"properties": {
+		"name": "Paradise Inn",
+		"amenity": "bar"
+	},
+	"geometry": {"type": "Point", "coordinates": [-81.7072207, 41.4322678]}},
+{"type": "Feature",
+	"properties": {
+		"addr:postcode": "44114",
+		"website": "http://davidsgrillandbar.com/",
+		"name": "David's Grill & Bar",
+		"payment:bitcoin": "yes",
+		"amenity": "bar",
+		"addr:street": "Lakeside Ave",
+		"addr:housename": "North Point Tower",
+		"addr:housenumber": "1001"
+	},
+	"geometry": {"type": "Point", "coordinates": [-81.6910207, 41.5070127]}},
+{"type": "Feature",
+	"properties": {
+		"source": "Self",
+		"name": "Rhonda and Mike's ",
+		"designation": "Bar",
+		"amenity": "bar"
+	},
+	"geometry": {"type": "Point", "coordinates": [-81.7858122, 41.4566938]}},
+{"type": "Feature",
+	"properties": {
+		"name": "Mandy's",
+		"amenity": "bar"
+	},
+	"geometry": {"type": "Point", "coordinates": [-81.7164943, 41.4284168]}},
+{"type": "Feature",
+	"properties": {
+		"name": "Clark Bar",
+		"amenity": "bar"
+	},
+	"geometry": {"type": "Point", "coordinates": [-81.6901028, 41.4699334]}},
+{"type": "Feature",
+	"properties": {
+		"name": "Illusions",
+		"amenity": "bar"
+	},
+	"geometry": {"type": "Point", "coordinates": [-81.6974361, 41.4347529]}},
+{"type": "Feature",
+	"properties": {
+		"name": "Lava Lounge",
+		"amenity": "bar"
+	},
+	"geometry": {"type": "Point", "coordinates": [-81.69069, 41.4759192]}},
+{"type": "Feature",
+	"properties": {
+		"name": "Tequila Ranch",
+		"amenity": "bar"
+	},
+	"geometry": {"type": "Point", "coordinates": [-81.6994441, 41.5009796]}},
+{"type": "Feature",
+	"properties": {
+		"wifi": "yes",
+		"name": "Euclid Tavern",
+		"amenity": "bar",
+		"addr:street": "Euclid Ave"
+	},
+	"geometry": {"type": "Point", "coordinates": [-81.6027138, 41.5111859]}},
+{"type": "Feature",
+	"properties": {
+		"name": "Sweethearts",
+		"amenity": "bar"
+	},
+	"geometry": {"type": "Point", "coordinates": [-81.665295, 41.5125186]}},
+{"type": "Feature",
+	"properties": {
+		"addr:postcode": "44106",
+		"website": "www.thefairmount.net",
+		"name": "The Fairmount",
+		"amenity": "bar",
+		"addr:street": "Fairmount Blvd.",
+		"addr:housenumber": "2448"
+	},
+	"geometry": {"type": "Point", "coordinates": [-81.5924831, 41.5007879]}},
+{"type": "Feature",
+	"properties": {
+		"name": "Murphy's Law",
+		"amenity": "bar"
+	},
+	"geometry": {"type": "Point", "coordinates": [-81.7227226, 41.4397069]}},
+{"type": "Feature",
+	"properties": {
+		"name": "La Cave du Vin",
+		"amenity": "bar"
+	},
+	"geometry": {"type": "Point", "coordinates": [-81.5802884, 41.5084072]}},
+{"type": "Feature",
+	"properties": {
+		"name": "Big Boss's Lounge & Restaurant",
+		"amenity": "bar"
+	},
+	"geometry": {"type": "Point", "coordinates": [-81.6386662, 41.4482452]}},
+{"type": "Feature",
+	"properties": {
+		"name": "Erie Tavern",
+		"amenity": "bar"
+	},
+	"geometry": {"type": "Point", "coordinates": [-81.6530398, 41.5219864]}},
+{"type": "Feature",
+	"properties": {
+		"wifi": "no",
+		"name": "Nightown",
+		"amenity": "bar"
+	},
+	"geometry": {"type": "Point", "coordinates": [-81.5952651, 41.501248]}},
+{"type": "Feature",
+	"properties": {
+		"website": "http://www.myxxcleveland.com/",
+		"name": "Myxx",
+		"amenity": "bar",
+		"old_name": "Jillian's"
+	},
+	"geometry": {"type": "Point", "coordinates": [-81.5923737, 41.5012601]}},
+{"type": "Feature",
+	"properties": {
+		"name": "Kenny's",
+		"amenity": "bar"
+	},
+	"geometry": {"type": "Point", "coordinates": [-81.7227245, 41.441244]}},
+{"type": "Feature",
+	"properties": {
+		"name": "Alcove Lounge",
+		"amenity": "bar"
+	},
+	"geometry": {"type": "Point", "coordinates": [-81.7238445, 41.440605]}},
+{"type": "Feature",
+	"properties": {
+		"name": "Larry's Tavern",
+		"amenity": "bar"
+	},
+	"geometry": {"type": "Point", "coordinates": [-81.7097849, 41.4261053]}},
+{"type": "Feature",
+	"properties": {
+		"name": "Club Generations",
+		"amenity": "bar"
+	},
+	"geometry": {"type": "Point", "coordinates": [-81.6882193, 41.4474]}},
+{"type": "Feature",
+	"properties": {
+		"name": "Memphis Tavern",
+		"amenity": "bar"
+	},
+	"geometry": {"type": "Point", "coordinates": [-81.7198606, 41.4401028]}},
+{"type": "Feature",
+	"properties": {
+		"name": "Thirsty Parrot",
+		"amenity": "bar"
+	},
+	"geometry": {"type": "Point", "coordinates": [-81.6855065, 41.4985019]}},
+{"type": "Feature",
+	"properties": {
+		"name": "Club Denison Inc",
+		"amenity": "bar"
+	},
+	"geometry": {"type": "Point", "coordinates": [-81.6997489, 41.450513]}},
+{"type": "Feature",
+	"properties": {
+		"name": "Mr. E's",
+		"amenity": "bar"
+	},
+	"geometry": {"type": "Point", "coordinates": [-81.7066659, 41.4397823]}},
+{"type": "Feature",
+	"properties": {
+		"name": "ABC Tavern",
+		"amenity": "bar"
+	},
+	"geometry": {"type": "Point", "coordinates": [-81.7048554, 41.4856629]}},
+{"type": "Feature",
+	"properties": {
+		"name": "Bob Golic's",
+		"amenity": "bar"
+	},
+	"geometry": {"type": "Point", "coordinates": [-81.6996064, 41.5011741]}},
+{"type": "Feature",
+	"properties": {
+		"name": "Rowley Inn",
+		"amenity": "bar"
+	},
+	"geometry": {"type": "Point", "coordinates": [-81.6880709, 41.46873]}},
+{"type": "Feature",
+	"properties": {
+		"name": "Dirty Dog",
+		"amenity": "bar"
+	},
+	"geometry": {"type": "Point", "coordinates": [-81.7087958, 41.4267594]}},
+{"type": "Feature",
+	"properties": {
+		"name": "Hotz",
+		"amenity": "bar"
+	},
+	"geometry": {"type": "Point", "coordinates": [-81.6873066, 41.4772984]}},
+{"type": "Feature",
+	"properties": {
+		"name": "Dive Bar",
+		"amenity": "bar"
+	},
+	"geometry": {"type": "Point", "coordinates": [-81.6999956, 41.5009747]}},
+{"type": "Feature",
+	"properties": {
+		"ref": "107",
+		"phone": "(216) 325-1120",
+		"building": "yes",
+		"name": "Bar Louie",
+		"amenity": "bar"
+	},
+	"geometry": {"type": "LineString", "coordinates": [[-81.5024868, 41.5032627], [-81.5021027, 41.5032644], [-81.5021013, 41.5034379], [-81.5024864, 41.5034368], [-81.5024868, 41.5032627]]}},
+{"type": "Feature",
+	"properties": {
+		"building": "yes",
+		"name": "Cinema Lounge",
+		"amenity": "bar"
+	},
+	"geometry": {"type": "LineString", "coordinates": [[-81.7219896, 41.4400371], [-81.7222163, 41.4400358], [-81.7222192, 41.4403167], [-81.7219925, 41.440318], [-81.7219896, 41.4400371]]}},
+{"type": "Feature",
+	"properties": {
+		"name": "Casablanca",
+		"amenity": "bar"
+	},
+	"geometry": {"type": "LineString", "coordinates": [[-81.7003204, 41.4389553], [-81.7002347, 41.4388998], [-81.7001653, 41.4389606], [-81.7001028, 41.4389293], [-81.7001861, 41.4388686], [-81.7000727, 41.438794], [-81.6999847, 41.4388425], [-81.699906, 41.4387679], [-81.7000982, 41.4386603], [-81.7004107, 41.438879], [-81.7003204, 41.4389553]]}},
+{"type": "Feature",
+	"properties": {
+		"building": "yes",
+		"smoking": "separated",
+		"name": "Gino's",
+		"amenity": "bar"
+	},
+	"geometry": {"type": "LineString", "coordinates": [[-81.6896897, 41.4521964], [-81.6896867, 41.4520236], [-81.6895142, 41.4520258], [-81.6895201, 41.4521585], [-81.6895811, 41.4521518], [-81.6895856, 41.4521964], [-81.6896897, 41.4521964]]}},
+{"type": "Feature",
+	"properties": {
+		"name": "Y's Guys",
+		"amenity": "bar"
+	},
+	"geometry": {"type": "LineString", "coordinates": [[-81.7067767, 41.4373279], [-81.7067771, 41.437212], [-81.7066035, 41.437215], [-81.7065887, 41.4373281], [-81.7067767, 41.4373279]]}},
+{"type": "Feature",
+	"properties": {
+		"name": "Dunlap's Country Corner",
+		"amenity": "bar"
+	},
+	"geometry": {"type": "LineString", "coordinates": [[-81.7044821, 41.4654281], [-81.7044853, 41.4653057], [-81.7042984, 41.4653075], [-81.7043086, 41.4654369], [-81.7044821, 41.4654281]]}},
+{"type": "Feature",
+	"properties": {
+		"name": "Steelyard Tavern",
+		"amenity": "bar"
+	},
+	"geometry": {"type": "LineString", "coordinates": [[-81.6970239, 41.433577], [-81.6967838, 41.4335787], [-81.6967827, 41.4334938], [-81.6970228, 41.4334921], [-81.6970239, 41.433577]]}},
+{"type": "Feature",
+	"properties": {
+		"building": "yes",
+		"amenity": "bar"
+	},
+	"geometry": {"type": "LineString", "coordinates": [[-81.681071, 41.5039207], [-81.6810458, 41.5038369], [-81.6806461, 41.503883], [-81.6806656, 41.503973], [-81.681071, 41.5039207]]}},
+{"type": "Feature",
+	"properties": {
+		"building": "yes",
+		"name": "Mike's",
+		"amenity": "bar"
+	},
+	"geometry": {"type": "LineString", "coordinates": [[-81.7006233, 41.4386561], [-81.7007622, 41.438545], [-81.700695, 41.4384982], [-81.7005608, 41.438611], [-81.7006233, 41.4386561]]}}
+]
+}
